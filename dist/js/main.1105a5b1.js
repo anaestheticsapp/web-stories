cb751f292fadeeaf892e726f2f001701b141745a
anaestheticsapp <49522559+anaestheticsapp@users.noreply.github.com>
2020-12-22 12:04:37 +0100

    updated stories url

--- dist/js/main.1105a5b1.js
+++ dist/js/main.1105a5b1.js
@@ -2695,49 +2695,49 @@ async function webShare(url, opts = {}) {
 
 const stories = [
   [
-    { url: './assets/stories/au-8.webp', text: 'Merry Christmas!', class: 'text' },
-    { url: './assets/stories/au-7.webp' },
-    { url: './assets/stories/au-6.webp' },
-    { url: './assets/stories/au-5.webp' },
-    { url: './assets/stories/au-4.webp', text: 'Hunter Valley', class: 'h1' },
-    { url: './assets/stories/au-3.webp' },
-    { url: './assets/stories/au-2.webp' },
-    { url: './assets/stories/au-1.webp', text: 'Australia', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-8.webp', text: 'Merry Christmas!', class: 'text' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-7.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-6.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-5.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-4.webp', text: 'Hunter Valley', class: 'h1' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/au-1.webp', text: 'Australia', class: 'label' },
   ],
   [
-    { url: './assets/stories/sp-4.webp' },
-    { url: './assets/stories/sp-3.webp' },
-    { url: './assets/stories/sp-2.webp' },
-    { url: './assets/stories/sp-1.webp', text: 'Scafell Pike', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/sp-4.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/sp-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/sp-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/sp-1.webp', text: 'Scafell Pike', class: 'label' },
   ],
   [
-    { url: './assets/stories/gc-4.webp' },
-    { url: './assets/stories/gc-3.webp' },
-    { url: './assets/stories/gc-2.webp', text: 'Grand Canyon', class: 'h1' },
-    { url: './assets/stories/gc-1.webp', text: 'Hoover Dam', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/gc-4.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/gc-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/gc-2.webp', text: 'Grand Canyon', class: 'h1' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/gc-1.webp', text: 'Hoover Dam', class: 'label' },
   ],
   [
-    { url: './assets/stories/manc-2.webp' },
-    { url: './assets/stories/manc-1.webp', text: 'Manchester', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/manc-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/manc-1.webp', text: 'Manchester', class: 'label' },
   ],
   [
-    { url: './assets/stories/san-4.webp' },
-    { url: './assets/stories/san-3.webp' },
-    { url: './assets/stories/san-2.webp' },
-    { url: './assets/stories/san-1.webp', text: 'San Francisco', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/san-4.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/san-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/san-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/san-1.webp', text: 'San Francisco', class: 'label' },
   ],
   [
-    { url: './assets/stories/van-6.webp' },
-    { url: './assets/stories/van-5.webp' },
-    { url: './assets/stories/van-4.webp' },
-    { url: './assets/stories/van-3.webp' },
-    { url: './assets/stories/van-2.webp' },
-    { url: './assets/stories/van-1.webp', text: 'Vancouver', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-6.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-5.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-4.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/van-1.webp', text: 'Vancouver', class: 'label' },
   ],
   [
-    { url: './assets/stories/yp-3.webp' },
-    { url: './assets/stories/yp-2.webp' },
-    { url: './assets/stories/yp-1.webp', text: 'Yosemite', class: 'label' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/yp-3.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/yp-2.webp' },
+    { url: 'https://stories.anaesthetics.app/assets/stories/yp-1.webp', text: 'Yosemite', class: 'label' },
   ],
 ];
 
@@ -3134,11 +3134,11 @@ class StoriesView extends LitElement {
 customElements.define(COMPONENT, StoriesView);
 
 (async function loadMaterialIcons() {
-  await import('./svg-icon-9a47b6f1.js');
+  await import('./svg-icon-16d05eb6.js');
 }());
 
 window.addEventListener('error', (e) => console.error(e));
 window.addEventListener('unhandledrejection', (e) => console.error(e.reason));
 
 export { LitElement as L, css as c, html as h };
-//# sourceMappingURL=main.c7cf6372.js.map
+//# sourceMappingURL=main.1105a5b1.js.map
